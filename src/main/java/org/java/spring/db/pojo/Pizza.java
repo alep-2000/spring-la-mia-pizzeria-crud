3376e300d93778ce3290b719b6c00572ba2bfd62
alep-2000 <alessiopiras056@gmail.com>
2023-11-30 12:15:02 +0100

    Classe pizza creata

--- src/main/java/org/java/spring/db/pojo/Pizza.java
+++ src/main/java/org/java/spring/db/pojo/Pizza.java
@@ -1,6 +1,71 @@
 package org.java.spring.db.pojo;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
 public class Pizza {
-
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(length = 60)
+	private String nome;
+	
+	@Column(columnDefinition = "TEXT")
+	private String descrizione;
+	
+	private String foto;
+	
+	private double prezzo;
+	
+	public Pizza() { }
+	public Pizza(String name, String descrizione, String foto, double prezzo) {
+		
+		setNome(nome);
+		setDescrizione(descrizione);
+		setFoto(foto);
+		setPrezzo(prezzo);
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescrizione() {
+		return descrizione;
+	}
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+	public String getFoto() {
+		return foto;
+	}
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+	public double getPrezzo() {
+		return prezzo;
+	}
+	public void setPrezzo(double prezzo) {
+		this.prezzo = prezzo;
+	}
+	@Override
+	public String toString() {
+		
+		return "[" + getId() + "] " + getNome() + " - " 
+				+ getFoto() + " (" + getPrezzo() + ")";
+	}
 }
