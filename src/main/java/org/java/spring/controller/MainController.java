01b100614ab2af9e4a3b327ad173cac8cee82e1f
alep-2000 <alessiopiras056@gmail.com>
2023-12-04 12:24:41 +0100

    Create e Store create

--- src/main/java/org/java/spring/controller/MainController.java
+++ src/main/java/org/java/spring/controller/MainController.java
@@ -7,9 +7,14 @@ import org.java.spring.db.service.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class MainController {
@@ -49,6 +54,38 @@ public class MainController {
 		model.addAttribute("pizza", pizza);
 		
 		return "pizza";
+	}
+	
+	@GetMapping("/pizzas/create")
+	public String createPizza(Model model) {
+		
+		Pizza pizza = new Pizza();
+		
+		model.addAttribute("pizza", pizza);
+		
+		return "pizza-form";
+	}
+	
+	@PostMapping("/pizzas/create")
+	public String storePizza(
+			Model model,
+			@Valid @ModelAttribute Pizza pizza, 
+			BindingResult bindingResult) {
+		
+		System.out.println("Pizza:\n" + pizza);
+		System.out.println("\n---------------\n");
+		System.out.println("Error:\n" + bindingResult);
+		
+		if (bindingResult.hasErrors()) {
+			
+			System.out.println(bindingResult);
+			model.addAttribute("pizza", pizza);
+			return "pizza-form";
+		}
+		
+		pizzaService.save(pizza);
+			
+		return "redirect:/";
 	}
 }
 
